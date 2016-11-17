9bac831a08075e99925b4260457b07f393756024
andrewlehmann <andrew@arlehmann.com>
2016-11-16 18:59:12 -0600

    added sendmessage functionality

--- auto_weather/sendmessage.py
+++ auto_weather/sendmessage.py
@@ -5,17 +5,25 @@ from twilio.rest import TwilioRestClient
 def twilio_creds(): #twillo credentials
 	sid = keyring.get_password("Twillosid", "sid")
 	auth = keyring.get_password("Twilloauth", "auth")
-	twilio_phone = keyring.get_password("Twiliophone", "phone")
-	my_phone = keyring.get_password("myphone", "phone")
 
 	return sid, auth#, twilio_phone, my_phone
 
-def create_message(t_phone, my_phone, ):
+def create_message(w_list):
 	t = TwilioRestClient(twilio_creds())
-	#message = t.messages.create(body = )
+	message = 'Current temp: %s \nLow: %s \nHigh: %s \nDescription: %s \n'\
+	 % (w_list[0], w_list[1], w_list[2], w_list[3])
+
+	return message
 
 
-def send_message():
+def send_message(t_phone, my_phone, msg):
+	t = TwilioRestClient(twilio_creds())
+	t.messages.create(body=msg, from_=t_phone, to=my_phone)
+
+def phone_numbers():
+	twilio_phone = keyring.get_password("Twiliophone", "phone")
+	my_phone = keyring.get_password("myphone", "phone")
+
 	
 
 
--- tests/weathertest.py
+++ tests/weathertest.py
@@ -5,7 +5,7 @@ class Test(unittest.TestCase):
 
 	def test_weather(self):
 		desc = 'Mostly clear, with a low around 62. Southeast wind around 5 mph becoming calm  in the evening. '
-		self.assertEqual(getweather.get_weather(29.8340,-95.4342),('73', '62', '83', desc))
+		self.assertEqual(getweather.get_weather(29.8340,-95.4342),['73', '62', '83', desc])
 
 	if __name__ == '__main__':
 		unittest.main()
