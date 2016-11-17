687dfe713b84683f87aa4f7feaba901428702acf
andrewlehmann <andrew@arlehmann.com>
2016-11-16 18:16:18 -0600

    unit test for weather working

--- tests/weathertest.py
+++ tests/weathertest.py
@@ -1,2 +1,10 @@
 import unittest
 from .context import getweather
+
+class Test(unittest.TestCase):
+	def weather_test(self):
+		desc = '''Mostly clear, with a low around 62. 
+			Southeast wind around 5 mph becoming calm  
+			in the evening. '''
+		self.assertEqual(get_weather(29.8340,-95.4342), \
+			('73', '62', '83', desc))
