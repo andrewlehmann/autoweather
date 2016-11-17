7e22569a38fa3f17ad8086e441e02c2021288a30
Andrew Lehmann <andrewlehmann@users.noreply.github.com>
2016-11-16 23:46:25 -0600

    Update sendmessage.py

--- auto_weather/sendmessage.py
+++ auto_weather/sendmessage.py
@@ -6,7 +6,7 @@ def twilio_creds(): #twillo credentials
 	sid = keyring.get_password("Twillosid", "sid")
 	auth = keyring.get_password("Twilloauth", "auth")
 
-	return sid, auth#, twilio_phone, my_phone
+	return sid, auth
 
 def create_message(w_list):
 	sid, auth = twilio_creds()
