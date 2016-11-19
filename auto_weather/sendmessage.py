7acfd63c7f02ca2dcbdeb07515056a22447be5a1
Andrew Lehmann <andrewlehmann@users.noreply.github.com>
2016-11-18 18:20:59 -0600

    reorder functions for readability

--- auto_weather/sendmessage.py
+++ auto_weather/sendmessage.py
@@ -5,15 +5,18 @@ from twilio.rest import TwilioRestClient
 def twilio_creds(): #twillo credentials
 	sid = keyring.get_password("Twillosid", "sid")
 	auth = keyring.get_password("Twilloauth", "auth")
-
 	return sid, auth
+
+def phone_numbers():
+	twilio_phone = keyring.get_password("Twiliophone", "phone")
+	my_phone = keyring.get_password("myphone", "phone")
+	return twilio_phone, my_phone
 
 def create_message(w_list):
 	sid, auth = twilio_creds()
 	t = TwilioRestClient(account=sid, token=auth)
 	message = 'Current temp: %s \nLow: %s \nHigh: %s \nDescription: %s \n'\
 	 % (w_list[0], w_list[1], w_list[2], w_list[3])
-
 	return message
 
 
@@ -22,11 +25,6 @@ def send_message(t_phone, my_phone, msg):
 	t = TwilioRestClient(sid, auth)
 	t.messages.create(body=msg, from_=t_phone, to=my_phone)
 	return
-
-def phone_numbers():
-	twilio_phone = keyring.get_password("Twiliophone", "phone")
-	my_phone = keyring.get_password("myphone", "phone")
-	return twilio_phone, my_phone
 
 	
 
