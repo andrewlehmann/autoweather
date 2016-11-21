2ce8f525f7b28196e90f3ab2e58461d676328d10
andrewlehmann <andrew@arlehmann.com>
2016-11-20 20:24:20 -0600

    add database functionality

--- auto_weather/main.py
+++ auto_weather/main.py
@@ -6,16 +6,20 @@ def job():
 	import sendmessage
 	import location
 	import getweather
+	import mongo
 	
 	ip = location.get_ip()
 	lat, lon = location.get_location(ip)
 	weather = getweather.get_weather(lat, lon)
 
+	db = mongo.get_database()
+	insert(db, weather)			#put info in database
+
 	msg = sendmessage.create_message(weather)
 	t_phone, m_phone = sendmessage.phone_numbers()
 	sendmessage.send_message(t_phone, m_phone, msg)
-	now = time.strftime("%c")
-	print 'message sent at %s' % now
+	today = time.strftime("%x")
+	print 'message sent on %s' % today
 	return
 
 
--- auto_weather/mongo.py
+++ auto_weather/mongo.py
@@ -2,23 +2,18 @@ import PyMongo
 import keyring
 
 
-def connection():
+def get_database():
 	username = keyring.get_password("MongoDB", "username")
 	password = keyring.get_password("MongoDB", "password")
 	connection_string = 'mongodb://%s:%s@ds159507.mlab.com:59507' % (username, password)
 	client = MongoClient(connection_string)
-	return client
+	db = client.weather
+	return db
 
 def insert(db, w_list):
-	weather = db.weather
-	result = db.weather.insert_one(
-		{
-			"Current Temperature": w_list[0],
-			"Low": w_list[1],
-			"High:" w_list[2],
-			"Description": w_list[3]
-		}
-	)
+	result = db.insert_one(w_list)
+	return
+
 
 
 
