913eda1a1d61d6a17f81db46b3a19cc01ab0ebf3
andrewlehmann <andrew@arlehmann.com>
2016-11-20 18:22:29 -0600

    add important if statement to unit test

--- tests/test_createmessage.py
+++ tests/test_createmessage.py
@@ -4,3 +4,6 @@ from .context import sendmessage
 class Test(unittest.TestCase):
 	def test_create_message(self):
 		self.assertEqual(sendmessage.create_message(5, 5, 10, 'lol'), 'Current temp: 5 \nLow: 5 \nHigh: 10 \nDescription: lol \n'))
+
+	if __name__ == '__main__':
+		unittest.main()
