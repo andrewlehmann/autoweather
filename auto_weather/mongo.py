7d2770943a0cc804ac1d737d9fc78ee25d5580ed
Andrew Lehmann <andrewlehmann@users.noreply.github.com>
2016-12-21 10:03:06 -0600

    documentation update

--- auto_weather/mongo.py
+++ auto_weather/mongo.py
@@ -2,19 +2,20 @@ import pymongo
 
 from keyring import get_password
 
-
+# instantiate mongodb connection
 def connection():
     connection_info = ['ds159507.mlab.com', 59507]
     client = pymongo.MongoClient(*connection_info)
     return client
 
-
+# retrieve hidden username and password via keyring library
+# which is used to hide sensitive information since code is public
 def auth(db):
     username = get_password("MongoDB", "username")
     password = get_password("MongoDB", "password")
     db.authenticate(username, password)
 
-
+# Takes in the dictionary of weather info, stores it in mongodb instance
 def insert(w_list):
     db = connection().weather
     auth(db)
