29acaae04ee7b36ff74ab5b2f8d25ef626542f59
Andrew Lehmann <andrewlehmann@users.noreply.github.com>
2016-12-21 10:00:11 -0600

    add comments

--- auto_weather/location.py
+++ auto_weather/location.py
@@ -2,12 +2,12 @@ import requests
 import json
 import urllib2
 
-
+# retrieve IP address of initiating machine
 def get_ip():
     ip = requests.get('http://api.ipify.org').text
     return ip
 
-
+#use IP address to return latitude and longitude
 def get_location():
     url = 'http://ipinfo.io/' + get_ip() + '/json'
     response = urllib2.urlopen(url)
