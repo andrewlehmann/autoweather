9560a885c1b9ba24e12b1fc169ae948278d2e68b
andrewlehmann <andrew@arlehmann.com>
2016-12-29 13:18:35 -0600

    comments in main now pep8

--- auto_weather/main.py
+++ auto_weather/main.py
@@ -8,21 +8,21 @@ import mongo
 
 
 def job():
-    
-    lat, lon = location.get_location() # retrieve latitude, longitude
-    weather = getweather.get_weather(lat, lon)  # retrieve weather info for gps coords
+
+    lat, lon = location.get_location()  # retrieve latitude, longitude
+    weather = getweather.get_weather(lat, lon)   # get weather via gps
     mongo.insert(weather)           # put info in database
-    msg = sendmessage.create_message(weather) #creates text message
+    msg = sendmessage.create_message(weather)  # creates text message
     sendmessage.send_message(msg)  # send text message
     print "Message sent"
 
 
 def main():
-    schedule.every().day.at("08:00").do(job) #run job() every day at 8AM
-    
-    while True: #run infinitely
+    schedule.every().day.at("08:00").do(job)  # run every day at 8AM
+
+    while True:  # run infinitely
         schedule.run_pending()
-        time.sleep(60) #check every 60 seconds to see if it's 8AM
+        time.sleep(60)  # check every 60 seconds to see if it's 8AM
 
 
 if __name__ == "__main__":
