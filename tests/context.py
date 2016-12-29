c846e4a584b42904c8dd883abe666154e121041d
andrewlehmann <andrew@arlehmann.com>
2016-12-29 14:36:42 -0600

    add avg temp overall

--- tests/context.py
+++ tests/context.py
@@ -5,5 +5,5 @@ sys.path.insert(0, os.path.abspath('/home/andrew/Desktop/Python/auto_weather'))
 
 from auto_weather import location
 from auto_weather import getweather
-from auto_weather import sendmessage
+from auto_weather import message
 from auto_weather import mongo
