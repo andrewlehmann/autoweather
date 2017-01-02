15fe2826235984ca825ad1bba952e32161b84e25
Andrew Lehmann <andrewlehmann@users.noreply.github.com>
2017-01-02 14:12:36 -0600

    reenable scheduling (prev. disabled for testing)

--- auto_weather/main.py
+++ auto_weather/main.py
@@ -13,7 +13,7 @@ def job():
     weather = getweather.get_weather(lat, lon)   # get weather via gps
     mongo.insert(weather)           # put info in database
 
-    avg_high, avg_low = mongo.avg_high_and_low(weather)
+    avg_high, avg_low = mongo.avg_high_and_low(weather) # Retrieve averages
     weather['Average_High'] = avg_high
     weather['Average_Low'] = avg_low
 
@@ -23,11 +23,11 @@ def job():
 
 
 def main():
-    #schedule.every().day.at("08:00").do(job)  # run every day at 8AM
-    job()
-    #while True:  # run infinitely
-    #    schedule.run_pending()
-    #    time.sleep(60)  # check every 60 seconds to see if it's 8AM
+    schedule.every().day.at("08:00").do(job)  # run every day at 8AM
+    # job()
+    while True:  # run infinitely
+        schedule.run_pending()
+        time.sleep(60)  # check every 60 seconds to see if it's 8AM
 
 
 if __name__ == "__main__":
