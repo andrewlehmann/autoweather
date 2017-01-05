fb491823c8856220487d95a3714015f9fc9f3880
Andrew Lehmann <andrewlehmann@users.noreply.github.com>
2017-01-05 11:29:06 -0600

    update documentation

--- tests/test_getlocation.py
+++ tests/test_getlocation.py
@@ -7,8 +7,8 @@ class Test(unittest.TestCase):
 
     def test_ip(self):
         self.assertEqual(
-            location.get_ip(), '129.7.0.27')
-
+            location.get_ip(), '129.7.0.27') # test is specific to own location
+                                             # must change if used elsewhere
     def test_location(self):
         self.assertEqual(
             location.get_location('129.7.0.27'), ('29.8340', '-95.4342'))
